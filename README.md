f5a59413db1539828486de9277d3f3a4135bd980
waterhumanB <dpfflsk45@gmail.com>
2022-09-23 17:53:59 +0900

    feat: pure-redux todolist

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 3. createStore 안에 있는 dispatch를 통해 action(무조건 객체, type 이여만 한다.)을 실행 시켜 reducer에서 state를 수정한다.
 4. subscribe를 통해 바뀐 state를 상태를 확인 할 수 있다.
 5. reducer안에 if문 보다 switch문을 사용하고, 액션이 실행되는 것들의 값을 변수로 만들어 놓고 사용한다.
+6. store의 값은 mutate 시키지 말고(state 변경 금지), 새로운 배열 값을 return 해야한다. 즉 state를 변경하는 것은 action에 의해서만 가능하다.
--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODOS = "ADD_TODO";
+const DELETE_TODOS = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const INITIAL_STATE = [];
 
-const reducer = (state = 0, action) => {
+const addToDo = (text) => {
+  return {
+    type: ADD_TODOS,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODOS,
+    id,
+  };
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ADD:
-      return state + 1;
-    case MINUS:
-      return state - 1;
+    case ADD_TODOS:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODOS:
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -22,19 +36,37 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = store.getState();
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
 };
 
-store.subscribe(onChange);
-
-const handleAdd = () => {
-  store.dispatch({ type: ADD });
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 };
 
-const handleMinus = () => {
-  store.dispatch({ type: MINUS });
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "Del";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
